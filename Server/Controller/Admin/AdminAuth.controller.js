6adcd3261cf8d39f992395d898e2e687d3b293dc
Tejas Dhodi <tejasdhodi77@gmail.com>
2024-03-29 23:12:29 +0530

    Improve Admin Auth

--- Server/Controller/Admin/AdminAuth.controller.js
+++ Server/Controller/Admin/AdminAuth.controller.js
@@ -2,16 +2,21 @@ const adminCredentials = require('../../Utils/AdminCredentials');
 
 const adminAuthController = async (req, res) => {
     try {
-        const { email, password } = req.body;
-        const admin = await adminCredentials.find((credential) => credential.email === email && credential.password === password)
+        const { userName, password } = req.body;
+        const admin = await adminCredentials.find((credential) => credential.userName === userName && credential.password === password)
 
         if (admin) {
-            res.status(200).json({
+            return res.status(200).json({
                 success: true,
                 message: "Login Successfull",
-                adminAuthToken: req.token
+                adminAuthToken: req.adminAuthToken
             });
-        } 
+        }  else {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Login Credentials"
+            });
+        }
     } catch (error) {
         console.error("Error in adminAuthController:", error);
         res.status(500).json({
--- Server/Utils/AdminCredentials.js
+++ Server/Utils/AdminCredentials.js
@@ -1,6 +1,6 @@
 const adminCredentials = [{
     id: 1,
-    email: process.env.ADMIN_USERNAME,
+    userName: process.env.ADMIN_USERNAME,
     password: process.env.ADMIN_PASSWORD
 }]
 
